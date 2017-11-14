e33ef4fe4ca4efeb9a65c372dfcd53ba5d42a746
v.liberov <v.liberov@itexpert.ru>
2017-11-14 15:01:33 +0300

    hello world via react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,15 @@
-console.log('hello world')
+import React from "react"
+import ReactDOM from "react-dom"
+
+class HelloWorld extends React.Component{
+    render(){
+        return (
+            <div>Hello world</div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.body
+)
